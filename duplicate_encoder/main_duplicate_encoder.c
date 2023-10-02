211707917b064abbe245708ad8ff5db01a122d8c
Gilbert Adikankwu <gilbert945@outlook.com>
2023-10-02 09:28:10 +0100

    fix: change test string

--- duplicate_encoder/main_duplicate_encoder.c
+++ duplicate_encoder/main_duplicate_encoder.c
@@ -6,13 +6,15 @@
  */
 int main(void)
 {
-	char *str = "Success";
+	char *str = "ijN8Q0}P7PR60<//N65SSZRTOPa2=((0?+Qlk%1S7T{PlU&a=>S>Sl^i1^)cZ{(}/50fT(==e} `Q4``W(MQYa)`))}}))";
 	char *newStr;
 
 
 	newStr = DuplicateEncoder(str);
 
 	printf("Encode string is %s\n", newStr);
+
+	free(newStr);
 
 	return (0);
 }
